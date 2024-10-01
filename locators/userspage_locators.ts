054f354edc6823bc1e3cee41b4e7c45418e1e2d0
Artem Belov <artem.belov@strv.com>
2024-10-01 15:37:10 +0200

    add Users page locators

---
+++ locators/userspage_locators.ts
@@ -0,0 +1,7 @@
+const usersTableHeaderText = 'Users'
+const usersSideNavigationId = 'VerticalNavigationMenuItem::tab_navigation1--0_0'
+const retoolGridUsersId = 'RetoolGrid::Main'
+const userNameColumnId = 'HeaderCellContents::Label::3bc01'
+const userFullNameColumnId = 'HeaderCellContents::Label::f048b'
+const retoolGridProfileLabel = 'Profile'
+const retoolGridClipsLabel = 'Clips'
